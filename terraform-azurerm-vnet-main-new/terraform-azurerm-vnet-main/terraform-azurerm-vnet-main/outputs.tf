6a3f1d182de3f60d3bdb55b5c4a16985bf40d337
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-04 14:45:17 +0100

    Add files via upload

---
+++ terraform-azurerm-vnet-main-new/terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/outputs.tf
@@ -0,0 +1,39 @@
+output "vnet_address_space" {
+  description = "The address space of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.address_space
+}
+
+output "vnet_guid" {
+  description = "The GUID of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.guid
+}
+
+output "vnet_id" {
+  description = "The id of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_location" {
+  description = "The location of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.location
+}
+
+output "vnet_name" {
+  description = "The Name of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "vmsubnet_name" {
+  description = "The id of vmsubnet created inside the newly created vNet"
+  value       = azurerm_subnet.vmsubnet.id
+}
+
+output "appsubnet_name" {
+  description = "The id of appsubnet created inside the newly created vNet"
+  value       = azurerm_subnet.appsubnet.id
+}
+
+output "dbsubnetsubnet_name" {
+  description = "The id of dbsubnet created inside the newly created vNet"
+  value       = azurerm_subnet.dbsubnet.id
+}
