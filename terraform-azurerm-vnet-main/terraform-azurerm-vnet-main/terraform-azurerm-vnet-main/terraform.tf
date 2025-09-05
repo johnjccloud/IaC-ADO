b920a4e5cf0dd7496fcfa7fea8f8241fd22d6df0
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-05 08:06:17 +0100

    push1

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
@@ -1,14 +1,27 @@
 terraform {
     required_providers {
+        
+        azuredevops = {
+          source  = "microsoft/azuredevops"
+          version = ">=0.1.0"
+        }
+
         azurerm = {
             source = "hashicorp/azurerm"
             version = "~> 3.0"
         }
+
+        azuread = {
+          source = "hashicorp/azuread"
+          version = "~> 1.0"
+        }
+    
         modtm = {
           source  = "Azure/modtm"
           version = "0.3.2"
         }
       }    
+    
     backend "azurerm" {
         use_azuread_auth = true
         key = "app.terraform.tfstate"
